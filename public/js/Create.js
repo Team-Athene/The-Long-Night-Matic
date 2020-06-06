eedf95d64b2f1a6e0e175681501afa6ea663137e
Mekha Krishnan M <mekhakrishnanm@gmail.com>
2020-06-06 15:11:26 +0800

    Java Script

---
+++ public/js/Create.js
@@ -0,0 +1,63 @@
+
+BasicGame.Create = function (game) {
+
+	this.mainButton = null;
+	// this.startButton = null;
+
+};
+
+BasicGame.Create.prototype = {
+
+	create: function () {
+
+        // this.add.sprite(0,0,'preloaderBackground');
+        console.log("InSide Create");
+        
+		this.background = this.add.sprite(0, 0, 'createBG');
+		this.background.width = window.innerWidth
+		this.background.height = window.innerHeight
+		this.add.text(window.innerWidth/2.2, window.innerHeight/1.7, '123456', {fill : 'yellow'});
+		// this.startButton = this.add.button(window.innerWidth/1.6, window.innerHeight/1.2, 'startGame', this.startGame, this, 2, 1, 0);
+		// this.startButton.width = 150
+		// this.startButton.height = 70
+		this.mainButton = this.add.button(window.innerWidth/3.5,  window.innerHeight/1.2, 'mainMenu', this.mainMenu, this, 2, 1, 0);
+		this.mainButton.width = 150
+		this.mainButton.height = 70
+		this.music = this.add.audio('got')
+		this.music.loop = true
+		this.music.autoplay = true
+		if (this.sound.usingWebAudio && this.sound.context.state !== 'running') {
+			this.input.onDown.addOnce(function () {
+				this.sound.context.resume();
+			}, this);
+		}
+	},
+
+	update: function () {
+
+		//	Do some nice funky main menu effect here
+
+	},
+
+	startGame: function (pointer) {
+
+		//	Ok, the Play Button has been clicked or touched, so let's stop the music (otherwise it'll carry on playing)
+		//this.music.stop();
+        console.log('Start');
+
+		//	And start the actual game
+		this.state.start('Quiz');
+
+    },
+    
+	mainMenu: function (pointer) {
+
+		//	Ok, the Play Button has been clicked or touched, so let's stop the music (otherwise it'll carry on playing)
+		//this.music.stop();
+        console.log('mainMenu');
+
+		//	And start the actual game
+		this.state.start('MainMenu');
+
+	}
+};
---
+++ public/js/MainMenu.js
@@ -0,0 +1,76 @@
+BasicGame.MainMenu = function (game) {
+	this.playButton = null
+	this.statButton = null
+	this.joinButton = null
+}
+
+BasicGame.MainMenu.prototype = {
+	create: function () {
+		// this.add.sprite(0,0,'preloaderBackground');
+		this.background = this.add.sprite(0, 0, 'preloaderBackground')
+		this.background.width = window.innerWidth
+		this.background.height = window.innerHeight
+		
+		// this.quizJson = this.add.json(0,0,'quizJson')
+		this.playButton = this.add.button(
+			window.innerWidth / 1.45,
+			window.innerHeight / 3,
+			'button',
+			this.createGame,
+			this,
+			2,
+			1,
+			0
+		)
+		this.playButton.width = 320
+		this.playButton.height = 150
+
+		this.joinButton = this.add.button(
+			window.innerWidth / 1.45,
+			window.innerHeight / 2,
+			'button1',
+			this.joinGame,
+			this,
+			2,
+			1,
+			0
+		)
+		this.joinButton.width = 320
+		this.joinButton.height = 150
+
+		this.statButton = this.add.button(
+			window.innerWidth / 1.45,
+			window.innerHeight / 1.5,
+			'button2',
+			this.myKingdom,
+			this,
+			2,
+			1,
+			0
+		)
+		this.statButton.width = 320
+		this.statButton.height = 150
+		this.music = this.add.audio('got')
+		this.music.loop = true
+		this.music.autoplay = true
+		if (this.sound.usingWebAudio && this.sound.context.state !== 'running') {
+			this.input.onDown.addOnce(function () {
+				// this.sound.context.resume();
+			}, this)
+		}
+	},
+
+	update: function () {
+		//	Do some nice funky main menu effect here
+	},
+
+	createGame: function (pointer) {
+		this.state.start('Create')
+	},	
+	joinGame: function (pointer) {
+		this.state.start('Join')
+	},
+	myKingdom: function (pointer) {
+		this.state.start('Login')
+	},
+}
