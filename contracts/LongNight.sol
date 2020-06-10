9f4b45f9558d9453133f25e5e9a9745848e560af
Dennis Sam <denniarems@gmail.com>
2020-06-10 20:44:14 +0530

    123

--- contracts/LongNight.sol
+++ contracts/LongNight.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.6.8;
 
 contract LongNight{
@@ -13,6 +14,7 @@ contract LongNight{
         address payable winner;
         uint star;
     }
+    
     mapping(uint=>Game)public long_night ;
     uint public id = 1;
     
@@ -24,15 +26,23 @@ contract LongNight{
     function join_game(uint _id) public {
         long_night[_id].first_user = msg.sender;
     }
-    function close_game(uint _id,address payable _winner,uint _star) public {
+    function close_game(uint _id,address payable _winner,uint _star,uint _status) public {
         long_night[_id].winner = _winner;
         long_night[_id].star = _star;
-        if(_star == 1){
-            _winner.transfer((long_night[_id].bet_amount+(long_night[_id].bet_amount/2)));
+        if(_status == 0){
+            long_night[_id].first_user.transfer(long_night[_id].bet_amount);
+        }else if(_status==1){
+            if(_star == 1){
+            _winner.transfer((long_night[_id].bet_amount+((long_night[_id].bet_amount/100)*50)));
+            admin.transfer((long_night[_id].bet_amount/100)*50);
         }else if(_star == 2){
-            _winner.transfer((long_night[_id].bet_amount+(long_night[_id].bet_amount/2)));
+            _winner.transfer((long_night[_id].bet_amount+((long_night[_id].bet_amount/100)*70)));
+            admin.transfer((long_night[_id].bet_amount/100)*30);
         }else if(_star == 3){
-            _winner.transfer((long_night[_id].bet_amount+(long_night[_id].bet_amount/2)));
+            _winner.transfer((long_night[_id].bet_amount+((long_night[_id].bet_amount/100)*90)));
+            admin.transfer((long_night[_id].bet_amount/100)*10);
         }
+        }
+
     }
 }
