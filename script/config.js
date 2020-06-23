af0a07e470b0d4af8596768049c5d87bb34020ba
Dennis Sam <denniarems@gmail.com>
2020-06-23 19:25:19 +0530

    bug fix

--- script/config.js
+++ script/config.js
@@ -1,11 +1,11 @@
-const relayHub = require('../build/gsn/RelayHub.json').address;
-const stakeManager = require('../build/gsn/StakeManager.json').address;
-const paymaster = require('../build/gsn/Paymaster.json').address;
+const relayHub = require('../build/gsn/RelayHub.json').address
+const stakeManager = require('../build/gsn/StakeManager.json').address
+const paymaster = require('../build/gsn/Paymaster.json').address
 module.exports = {
-  config: {
-    contract: '0xAB08E3d885608fDF1f6C7306803CA19a90b16B90',
-    relayHub,
-    stakeManager,
-    paymaster,
-  },
-};
+	config: {
+		contract: '0xb214D868d7DBFbAa56e179432c8B14B00b1c2ec9',
+		relayHub,
+		stakeManager,
+		paymaster,
+	},
+}
--- script/index.js
+++ script/index.js
@@ -3,73 +3,75 @@ let account
 let LN, LongNight
 let netId
 import Web3 from 'web3'
-import {
-    config
-} from './config'
+import { config } from './config'
 const Gsn = require('@opengsn/gsn/dist/src/relayclient/')
-const configureGSN = require('@opengsn/gsn/dist/src/relayclient/GSNConfigurator').configureGSN
+const contractABI = require('../public/assets/build/LongNight.json')
+const configureGSN = require('@opengsn/gsn/dist/src/relayclient/GSNConfigurator')
+	.configureGSN
 const RelayProvider = Gsn.RelayProvider
 window.addEventListener('load', async () => {
-    if (typeof window.ethereum !== 'undefined') {
-        window.web3 = new Web3(ethereum)
-        const accounts = await ethereum.enable()
-        ethereum.autoRefreshOnNetworkChange = false
-        account = accounts[0]
-        console.log('Log: account', account)
-        const gsnConfig = configureGSN({
-            relayHubAddress: config.relayHub,
-            stakeManagerAddress: config.stakeManager,
-            methodSuffix: '_v4',
-            jsonStringifyRequest: true,
-            chainId: 5777,
-            paymasterAddress: config.paymaster,
-            gasPriceFactorPercent: 70,
-            relayLookupWindowBlocks: 1e5
-        })
-        var provider = new RelayProvider(web3.currentProvider, gsnConfig)
-        web3.setProvider(provider)
-    }
-    LN = new window.web3.eth.Contract(
-        contractABI.abi,
-        config.contract
-    )
+	if (typeof window.ethereum !== 'undefined') {
+		window.web3 = new Web3(ethereum)
+		const accounts = await ethereum.enable()
+		ethereum.autoRefreshOnNetworkChange = false
+		account = accounts[0]
+		console.log('Log: account', account)
+		const gsnConfig = configureGSN({
+			relayHubAddress: config.relayHub,
+			stakeManagerAddress: config.stakeManager,
+			methodSuffix: '_v4',
+			jsonStringifyRequest: true,
+			chainId: 5777,
+			paymasterAddress: config.paymaster,
+			gasPriceFactorPercent: 70,
+			relayLookupWindowBlocks: 1e5,
+		})
+		var provider = new RelayProvider(web3.currentProvider, gsnConfig)
+		web3.setProvider(provider)
+	}
+	LN = new window.web3.eth.Contract(contractABI.abi, config.contract)
 })
 window.ethereum.on('accountsChanged', function (accounts) {
-    // Time to reload your interface with accounts[0]!
-    account = accounts[0]
-    console.log('Log: account', account)
+	// Time to reload your interface with accounts[0]!
+	account = accounts[0]
+	console.log('Log: account', account)
 })
 
 window.createGame = async (userAddres) => {
-    try {
-        console.log('Log: createGame -> account', account)
-        // console.log('Log: createGame -> LN.methods', LN.methods)
-        const create_res = await LN.methods
-            .create_game()
-            .send({
-                from: account,
-                gas: 6000000,
-            })
-        // const create_res = await LN.methods.id().call({ from: account })
-        console.log('Log: createGame -> create_res', create_res)
+	try {
+		console.log('Log: createGame -> account', account)
+		// console.log('Log: createGame -> LN.methods', LN.methods)
+		const create_res = await LN.methods.create_game().send({
+			from: account,
+			gas: 6000000,
+			value: 1000000000,
+		})
+		// const create_res = await LN.methods.id().call({ from: account })
+		console.log('Log: createGame -> create_res', create_res)
 
-        window.socket.emit('create-game', userAddres)
-    } catch (error) {
-        console.log('Log: createGame -> error', error)
-    }
+		window.socket.emit('create-game', userAddres)
+	} catch (error) {
+		console.log('Log: createGame -> error', error)
+	}
 }
 
-window.joinGame = (room, userAddress) => {
-    window.socket.emit('join-game', {
-        room,
-        userAddress
-    })
+window.joinGame = async (room, userAddress) => {
+	const join_res = await LN.methods.join_game(parseInt(room)).send({
+		from: account,
+		gas: 6000000,
+		value: 1000000000,
+	})
+	console.log('Log: window.joinGame -> join_res', join_res)
+	window.socket.emit('join-game', {
+		room,
+		userAddress,
+	})
 }
 
 window.closeGame = (room) => {
-    window.socket.emit('close-quiz', {
-        room
-    })
+	window.socket.emit('close-quiz', {
+		room,
+	})
 }
 
 // window.socket.on('ans-and-next-quiz', (data) => {
@@ -83,5 +85,5 @@ window.closeGame = (room) => {
 window.claimAmount = () => {}
 
 window.checkAnswer = (data) => {
-    window.socket.emit('check-quiz', data)
+	window.socket.emit('check-quiz', data)
 }
